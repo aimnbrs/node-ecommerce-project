e91230e9dd35ce6e881c5f6d4da83e5eec283ce0
aimnbrs <aimn.brs@gmail.com>
2021-04-23 17:25:59 -0400

    adjusement

--- DB/model/orderModel.js
+++ DB/model/orderModel.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const ProductItemsSchema = mongoose.Schema({
-    quantity : {type : Number, required : true},
-    model: { type: String, required: true },
-    url: { type: String, required: true },
-    price: { type: String, required: true },
-    product : {
-        _id : {
-            type :  mongoose.Schema.Types.ObjectId,
-            ref : "Product", 
-            required : true}
-    }
-})
+// const ProductItemsSchema = mongoose.Schema({
+//     quantity : {type : Number, required : true},
+//     product : {
+//             type :  mongoose.Schema.Types.ObjectId,
+//             ref : "Product",
+//             required : true
+//     }
+// })
 
-const adresseSchema = mongoose.Schema({
-    street : {type : String, required : true},
-    city: { type: String, required: true },
-    postalCode: { type: String, required: true },
-    country : { type: String, required: true },
-})
+// const adresseSchema = mongoose.Schema({
+//     street : {type : String, required : true},
+//     city: { type: String, required: true },
+//     postalCode: { type: String, required: true },
+//     country : { type: String, required: true },
+// })
 
 const orderSchema = mongoose.Schema({
-    _id : mongoose.Schema.Types.ObjectId,
-    isPayed : {type : Boolean},
-    isdelivered : {type : Boolean, required : true},
-    delivredAt : {type : Date},
-    productsCost : {type : Number , requied : true},
-    deliveryCost : {type : Number , requied : true},
-    taxes : {type : Number, required : true},
-    total : {type : Number, required : true},
-    adresse : adresseSchema,
-    productItems : ProductItemsSchema,
-})
+  quantity : {type : Number, default : 1},
 
-const orderModel = mongoose.model('Order', orderSchema);
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  // isPayed : {type : Boolean},
+  // isdelivered : {type : Boolean, required : true},
+  // delivredAt : {type : Date},
+  // productsCost : {type : Number , requied : true},
+  // deliveryCost : {type : Number , requied : true},
+  // taxes : {type : Number, required : true},
+  // total : {type : Number, required : true},
+  // adresse : adresseSchema,
+});
+
+const orderModel = mongoose.model("Order", orderSchema);
 module.exports = orderModel;
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,77 +1,69 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Order = require ('./../DB/model/orderModel');
-const mongoose = ('mongoose');
+const Order = require("./../DB/model/orderModel");
+const mongoose = "mongoose";
 
-router.post('/', async (req, res) => {
-    const neworder = new Order ({
-        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-        isPayed : req.body.isPayed,
-        isdelivered : req.body.isdelivered,
-        delivredAt : req.body.delivredAt,
-        productsCost : req.body.productsCost,
-        deliveryCost : req.body.deliveryCost,
-        taxes : req.body.taxes,
-        total : req.body.total,
-        adresse : req.body.adresse,
-        productItems : req.body.productItems,
-    })
-    const neworderCreat = await neworder.save();
-    res.status(201).send({message : "new order created", neworderCreat});
+router.post("/", async (req, res) => {
+  console.log(req.body);
+  const newOrder = new Order({
+    user_id: req.body.user_id,
+    product: req.body.product,
+    quantity : req.body.quantity,
+    // isPayed : req.body.isPayed,
+    // isdelivered : req.body.isdelivered,
+    // delivredAt : req.body.delivredAt,
+    // productsCost : req.body.productsCost,
+    // deliveryCost : req.body.deliveryCost,
+    // taxes : req.body.taxes,
+    // total : req.body.total,
+    // adresse : req.body.adresse,
+  });
+  const newOrderCreat = await newOrder.save();
+  res.status(201).send({ message: "new order created", newOrderCreat });
 });
 
 
 
-router.get("/", async(req, res) => {
-    try {
-        const orders = await Order.find(req.query).populate('user');
-        res.status(201).send(orders);
-    } catch(err) {
-        res.send(err)
-        console.log(err.message)
-    }
-})
-
-router.get("/", async(req, res) => {
-    try {
-        const orders = await Order.find(req.query);
-        res.status(201).send(orders);
-    } catch(err) {
-        res.send(err)
-        console.log(err.message)
-    }
-})
-
-router.get("/myaccount", async(req, res) => {
-    try {
-        const orders = await Order.find({user :req.user._id});
-        res.status(201).send(orders);
-    } catch(err) {
-        res.send(err)
-        console.log(err.message)
-    }
-})
+router.get("/", async (req, res) => {
+  const user_id = {user_id : req.query.user_id};
+  console.log('userid',user_id)
+  try {
+    const orders = await Order.find({ ...user_id }).populate("product");
+    res.status(201).send(orders);
+  } catch (err) {
+    res.send(err);
+    console.log(err.message);
+  }
+});
 
 
 
-router.patch("/:id", async(req, res) => {
-    try {
-        const orders = await Order.findByIdAndUpdate(req.params.id,req.body,{new : true, useFindAndModify : false});
-        res.status(201).send(orders);
-    } catch(err) {
-        res.send(err)
-        console.log(err.message)
-    }
-})
+router.patch("/:id", async (req, res) => {
+  try {
+    console.log(req.body)
+    const quantity = {quantity : req.body.valueUpate}
+    const orders = await Order.findByIdAndUpdate(req.params.id, quantity, {
+      new: true,
+      useFindAndModify: false,
+    });
+    res.status(201).send(orders);
+  } catch (err) {
+    res.send(err);
+    console.log(err.message);
+  }
+});
 
-router.delete("/:id", async(req, res) => {
-    try {
-        const orders = await Order.findByIdAndDelete(req.params.id,{useFindAndModify : false})
-        res.status(201).send(orders);
-    } catch(err) {
-        console.log(err.message)
-        res.send(err)
-    }
-})
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log("deleteAction")
+    const orders = await Order.findByIdAndDelete(req.params.id, {
+      useFindAndModify: false,
+    });
+    res.status(201).send(orders);
+  } catch (err) {
+    console.log(err.message);
+    res.send(err);
+  }
+});
 
 module.exports = router;
